bca77f87c0a8f661d3053edf96641e1a88f92637
Jerscape <jeremydutton5@gmail.com>
2023-07-14 12:18:29 -0400

    lint and fix minor errors

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,29 +1,27 @@
 const request = require('request');
-const fs = require('fs');
 
-
-const arguments = process.argv
+const arguments = process.argv;
 const breed = arguments[2];
-let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`
+let url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-const {body} = request(url, (error, response, body) => {
-  if(error){
-    console.log(error)
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
   } else {
 
-    let data = JSON.parse(body)
+    let data = JSON.parse(body);
  
-    if(data.length === 0){
-      console.log("I'm sorry that is not an available breed in the database.")
+    if (data.length === 0) {
+      console.log("I'm sorry that is not an available breed in the database.");
 
     } else {
       //let data = JSON.parse(body)
-      let {description} = data[0]
-      console.log(description)
+      let {description} = data[0];
+      console.log(description);
 
     }
 
   }
 
-})
+});
 
